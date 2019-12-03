eb091ca2a8de7ea7c3341fb22ae06ffa83d8c6fc
Joan F <joanfoubertetude@gmail.com>
2019-12-03 19:29:24 +0100

    NumberRamdon

--- moreLess.c
+++ moreLess.c
@@ -5,5 +5,24 @@
 
 int main()
 {
-    
+    int nombreMystere = 0, nombreEntre = 0;
+    const int MAX = 100, MIN = 1;
+    srand(time(NULL));
+    nombreMystere = (rand() % (MAX - MIN + 1)) - 1;
+    do
+    {
+        printf("Trouvez le nombre mystere compris entre 1 et 100 : \n");
+        scanf("%d", &nombreEntre);
+        if(nombreMystere < nombreEntre)
+        {
+            printf("c'est moins\n");
+        }else if( nombreMystere > nombreEntre)
+        {
+            printf("C'est plus !\n");
+        }else
+        {
+            printf("Bravo tu as trouver !\n");
+        }
+
+    }while(nombreMystere != nombreEntre);
 }
