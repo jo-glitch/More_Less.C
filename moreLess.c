916809d925e4825d5c2c4d6743bd2860eae508e0
Joan F <joanfoubertetude@gmail.com>
2019-12-03 19:47:56 +0100

    counter

--- moreLess.c
+++ moreLess.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
+    // fct pour compter les nombres de coup qu'on initialise à 1 pour le 1er coup
+int comptCoup(int coup){
+    coup = 1;
+    return coup;
+}
 int main()
 {
     int nombreMystere = 0, nombreEntre = 0;
     const int MAX = 100, MIN = 1;
     srand(time(NULL));
     nombreMystere = (rand() % (MAX - MIN + 1)) - 1;
-    int compteur = 1;
+    // variable pour la ftc comptCoup
+     int compteur = 0, plusUn =0;
+     compteur = comptCoup(plusUn);
+    // boucle du plus ou moins et du compteur
     do
     {
         printf("Nombre de tentative(s) : %d\n", compteur);
@@ -25,6 +31,8 @@ int main()
         {
             printf("Bravo tu as trouver, au bout de %d tentative(s) !\n", compteur);
         }
-        compteur++;
+        // chaque appel de boucle + 1 au compteur
+         compteur++;
+         // tant que nombreMystere N'EST pas egale a nombreEntre rejouer la boucle
     }while(nombreMystere != nombreEntre);
 }
